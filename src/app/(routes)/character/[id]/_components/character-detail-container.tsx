86c2d5d15c8a57b4be6664b418c9326711fa8ac9
stakbucks <dhassidu11@gmail.com>
2024-02-13 13:30:54 +0000

    feat: 캐릭터 상세 데이터 패칭 로직 구현

---
+++ src/app/(routes)/character/[id]/_components/character-detail-container.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import getCharacterDetail from '@/services/character/getCharacterDetail';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { usePathname } from 'next/navigation';
+import { getCookie } from 'cookies-next';
+import CharacterDetail from '@/components/character-detail';
+
+export default function CharacterDetailContainer() {
+    const pathname = usePathname();
+    const characterId = Number(pathname.split('character/')[1]);
+
+    const { data: character } = useSuspenseQuery({
+        queryKey: ['character', characterId],
+        queryFn: () => getCharacterDetail({ accessToken: `${getCookie('accessToken')}`, characterId }),
+        staleTime: 1000 * 60 * 5,
+    });
+
+    return <CharacterDetail character={character} className="mt-[24px]" />;
+}
