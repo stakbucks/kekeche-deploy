ff1e064c164a2a99f0b039566389f6c6ce67baa0
stakbucks <dhassidu11@gmail.com>
2024-04-02 13:40:39 +0000

    feat: ReactQueryStreamedHydration 사용

--- src/context/query-client-context.tsx
+++ src/context/query-client-context.tsx
@@ -2,18 +2,39 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
 type Props = {
     children: React.ReactNode;
 };
 
+function makeQueryClient() {
+    return new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 60 * 1000,
+            },
+        },
+    });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+    if (typeof window === 'undefined') {
+        return makeQueryClient();
+    } else {
+        if (!browserQueryClient) browserQueryClient = makeQueryClient();
+        return browserQueryClient;
+    }
+}
+
 export default function QueryClientContext({ children }: Props) {
-    const [queryClient] = useState(() => new QueryClient());
+    const queryClient = getQueryClient();
 
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
+            <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
             <ReactQueryDevtools />
         </QueryClientProvider>
     );
