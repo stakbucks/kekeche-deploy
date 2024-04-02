2265b924e8b0d9b613a8a99333ed95f42db6ba58
stakbucks <dhassidu11@gmail.com>
2024-04-02 13:54:27 +0000

    테스트용 커밋

--- src/context/query-client-context.tsx
+++ src/context/query-client-context.tsx
@@ -2,39 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+import { useState } from 'react';
 
 type Props = {
     children: React.ReactNode;
 };
 
-function makeQueryClient() {
-    return new QueryClient({
-        defaultOptions: {
-            queries: {
-                staleTime: 60 * 1000,
-            },
-        },
-    });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-    if (typeof window === 'undefined') {
-        return makeQueryClient();
-    } else {
-        if (!browserQueryClient) browserQueryClient = makeQueryClient();
-        return browserQueryClient;
-    }
-}
-
 export default function QueryClientContext({ children }: Props) {
-    const queryClient = getQueryClient();
+    const [queryClient] = useState(() => new QueryClient());
 
     return (
         <QueryClientProvider client={queryClient}>
-            <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+            {children}
             <ReactQueryDevtools />
         </QueryClientProvider>
     );
--- src/services/character/getCharacters.ts
+++ src/services/character/getCharacters.ts
@@ -15,7 +15,7 @@ export type GetCharactersRequest = {
     accessToken?: string;
 };
 
-export default async function getCharacters(request: GetCharactersRequest): Promise<GetCharactersResponse> {
+export default async function getCharacters(request: GetCharactersRequest): Promise<GetCharactersResponse | undefined> {
     try {
         const res = await fetch(
             `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/v1/character/member/${request.memberId}`,
